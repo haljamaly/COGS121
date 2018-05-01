3aa25361fddee4abdc50986e012cb8331b367566
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-04-30 19:47:55 -0700

    Removed fakeDatabase

--- create_database.js
+++ create_database.js
@@ -21,7 +21,7 @@ db.serialize(() => {
   // create a new database table:
   db.run("CREATE TABLE users (uid text PRIMARY KEY, name text NOT NULL, img text)");
   db.run("CREATE TABLE wishlist (user text NOT NULL, location text NOT NULL)");
-  db.run("CREATE TABLE posts (pid text PRIMARY KEY, title text NOT NULL, img text, time text, author text NOT NULL, location text NOT NULL, content text NOT NULL)");
+  db.run("CREATE TABLE posts (pid text PRIMARY KEY, title text NOT NULL, img text, time text, author text NOT NULL, location text NOT NULL COLLATE NOCASE, content text NOT NULL)");
   db.run("CREATE TABLE locations (title text PRIMARY KEY, score integer NOT NULL)");
 
   // insert 3 rows of data:
@@ -33,7 +33,7 @@ db.serialize(() => {
   db.run("INSERT INTO wishlist VALUES ('3', 'New York')");
 
   db.run("INSERT INTO posts VALUES ('1','Mount Fuji is good!', 'https://news.cruise1st.co.uk/wp-content/uploads/2018/03/header-mount-fuji.jpg', '2018-4-22T10:25:43.511', '1', 'Mount Fuji', '<p>Yo, Mount Fuji is awesome.</p>')");
-  db.run("INSERT INTO posts VALUES ('2','Los Angeles is lit!', 'https://amp.businessinsider.com/images/5aa2d4bb06b2b72a008b45c3-750-563.jpg','2018-4-22T14:25:43.511Z','3', 'New York', '<p>Los Angeles is beautiful and alive.</p>')");
+  db.run("INSERT INTO posts VALUES ('2','Los Angeles is lit!', 'https://amp.businessinsider.com/images/5aa2d4bb06b2b72a008b45c3-750-563.jpg','2018-4-22T14:25:43.511Z','3', 'Los Angeles', '<p>Los Angeles is beautiful and alive.</p>')");
 
   db.run("INSERT INTO locations VALUES ('Mount Fuji', 1 )");
   db.run ("INSERT INTO locations VALUES ( 'Los Angeles', 1)");
