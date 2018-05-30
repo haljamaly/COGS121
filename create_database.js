c1b8999f8dee27d12bc849ec6fa17bcdbe52cd24
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-05-29 17:04:50 -0700

    milestone 6

--- create_database.js
+++ create_database.js
@@ -41,8 +41,8 @@ db.serialize(() => {
   db.run("INSERT INTO visited VALUES (3, 'Mount Fuji')");
   db.run("INSERT INTO visited VALUES (2, 'Mount Fuji')");
 
-  db.run("INSERT INTO posts VALUES (1,'Mount Fuji is good!', 'https://news.cruise1st.co.uk/wp-content/uploads/2018/03/header-mount-fuji.jpg', '2018-04-22T10:25:43.511', 1, 'Hasan', 'Mount Fuji', '35.3605555', '138.72777769999993', '<p>Yo, Mount Fuji is awesome.</p>')");
-  db.run("INSERT INTO posts VALUES (2,'Los Angeles is lit!', 'https://amp.businessinsider.com/images/5aa2d4bb06b2b72a008b45c3-750-563.jpg','2018-04-22T14:25:43.511Z',2, 'Shuyuan', 'Los Angeles', '34.0522342', '-118.2436849', '<p>Los Angeles is beautiful and alive.</p>')");
+  db.run("INSERT INTO posts VALUES (1,'Mount Fuji is good!', 'https://news.cruise1st.co.uk/wp-content/uploads/2018/03/header-mount-fuji.jpg', '2018-04-22T10:25:43.511', 1, 'Hasan', 'Mount Fuji, Kitayama, Fujinomiya, Shizuoka Prefecture, Japan', '35.3605555', '138.72777769999993', '<p>Yo, Mount Fuji is awesome.</p>')");
+  db.run("INSERT INTO posts VALUES (2,'Los Angeles is lit!', 'https://amp.businessinsider.com/images/5aa2d4bb06b2b72a008b45c3-750-563.jpg','2018-04-22T14:25:43.511Z',2, 'Shuyuan', 'Los Angeles, CA, USA', '34.0522342', '-118.2436849', '<p>Los Angeles is beautiful and alive.</p>')");
 
   db.run("INSERT INTO locations VALUES ('Mount Fuji', 3)");
   db.run ("INSERT INTO locations VALUES ('Los Angeles', 2)");
